998fca00c9f9a46d7cb0aa36281f9a06a47ec7a5
tomo lance miamoto <lancelot@tommynovember7.com>
2020-05-08 08:55:13 +0900

    Improve local node handling

--- docker/bin/project-init.sh
+++ docker/bin/project-init.sh
@@ -21,6 +21,8 @@ sed "s/wp\.admin/${SITE_NAME}.admin/g" | tee docker/nginx/project.conf | grep -n
 printf "\nCreating '.bashrc' into docker/: \n\n" && \
 sed "s/wp\.local/${SITE_NAME}.local/g" docker/.bashrc.dist | \
 sed "s/wp\.admin/${SITE_NAME}.admin/g" | tee docker/.bashrc | grep -n "${SITE_NAME}" && \
+sed "s/wp\.local/${SITE_NAME}.local/g" docker/.env.dist | \
+sed "s/wp\.admin/${SITE_NAME}.admin/g" | tee docker/.env | grep -n "${SITE_NAME}" && \
 printf "\nCreating 'docker-compose.yaml' \n\n" && \
 sed "s/wp-/${SITE_NAME}-/g" docker-compose.yaml.dist | tee docker-compose.yaml | grep -n "${SITE_NAME}" && \
 git clone --recurse-submodules $1 project && \
