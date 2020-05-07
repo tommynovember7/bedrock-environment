28f1ea17d02b74daea53b686f62fb7f51ec18a9b
tomo lance miamoto <lancelot@tommynovember7.com>
2020-05-08 08:55:13 +0900

    Improve local node handling

--- docker/bin/project-init.sh
+++ docker/bin/project-init.sh
@@ -18,9 +18,9 @@ create_cert ${SITE_NAME} && \
 printf "\nCreating 'project.conf' into docker/nginx/: \n\n" && \
 sed "s/wp\.local/${SITE_NAME}.local/g" docker/nginx/project.conf.dist | \
 sed "s/wp\.admin/${SITE_NAME}.admin/g" | tee docker/nginx/project.conf | grep -n "${SITE_NAME}" && \
-printf "\nCreating '.bashrc' into docker/: \n\n" && \
-sed "s/wp\.local/${SITE_NAME}.local/g" docker/.bashrc.dist | \
-sed "s/wp\.admin/${SITE_NAME}.admin/g" | tee docker/.bashrc | grep -n "${SITE_NAME}" && \
+printf "\nCreating '.bashrc' into docker/: \n" && \
+cat docker/.bashrc.dist > docker/.bashrc && \
+printf "\nCreating '.env' into docker/: \n\n" && \
 sed "s/wp\.local/${SITE_NAME}.local/g" docker/.env.dist | \
 sed "s/wp\.admin/${SITE_NAME}.admin/g" | tee docker/.env | grep -n "${SITE_NAME}" && \
 printf "\nCreating 'docker-compose.yaml' \n\n" && \
