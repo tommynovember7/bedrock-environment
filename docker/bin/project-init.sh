60f1939bb88892c4e1d09a5ef70555b04005a6d6
tomo lance miamoto <lancelot@tommynovember7.com>
2020-05-16 07:48:04 +0900

    Fix ACF PRO issue when setting up project

--- docker/bin/project-init.sh
+++ docker/bin/project-init.sh
@@ -9,6 +9,11 @@ if [[ -z "$2" ]]; then
 else
   SITE_NAME="$2"
 fi
+if [[ -z "$3" ]]; then
+  ACF_PRO_KEY=""
+else
+    ACF_PRO_KEY=\'"$3"\'
+fi
 create_cert () {
   mkcert -cert-file docker/nginx/ssl/cert.pem -key-file docker/nginx/ssl/key.pem \
   "$1.local" "$1.admin" mailcatcher.local nginx mysql localhost 127.0.0.1 0.0.0.0
@@ -21,8 +26,10 @@ sed "s/wp\.admin/${SITE_NAME}.admin/g" | tee docker/nginx/project.conf | grep -n
 printf "\nCreating '.bashrc' into docker/: \n" && \
 cat docker/.bashrc.dist > docker/.bashrc && \
 printf "\nCreating '.env' into docker/: \n\n" && \
-sed "s/wp\.local/${SITE_NAME}.local/g" docker/.env.dist | \
+sed "s/ACF_PRO_KEY=/ACF_PRO_KEY=${ACF_PRO_KEY}/g" docker/.env.dist | \
+sed "s/wp\.local/${SITE_NAME}.local/g" | \
 sed "s/wp\.admin/${SITE_NAME}.admin/g" | tee docker/.env | grep -n "${SITE_NAME}" && \
+cat docker/.env | grep -n ACF_PRO_KEY && \
 printf "\nCreating 'docker-compose.yaml' \n\n" && \
 sed "s/wp-/${SITE_NAME}-/g" docker-compose.yaml.dist | tee docker-compose.yaml | grep -n "${SITE_NAME}" && \
 git clone --recurse-submodules $1 project && \
